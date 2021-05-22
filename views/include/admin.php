dd9a8942ab2d0c629e19c508a43d089bc1667b08
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-05-22 17:05:48 +0200

    Added styling to admin page

--- views/include/admin.php
+++ views/include/admin.php
@@ -1,8 +1,8 @@
-<div>
-    <a href="<?php echo SERVER_ROOT?>/admin/create">
-        <button class="btn btn-primary">Lägg till ny produkt</button>
+<div class="col-12 d-flex justify-content-center mb-5">
+    <a href="<?php echo SERVER_ROOT ?>/admin/create">
+        <button class="btn btn-primary mr-2">Lägg till ny produkt</button>
     </a>
-    <a href="<?php echo SERVER_ROOT?>/admin/orders">
+    <a href="<?php echo SERVER_ROOT ?>/admin/orders">
         <button class="btn btn-primary">Ordrar</button>
     </a>
 </div>
@@ -11,19 +11,22 @@
 
 $serverRoot = SERVER_ROOT;
 
-echo "<ul class='mx-auto'>";
 
 foreach ($products as $product) {
-    $li = "<li>$product[name]
-                <a href='$serverRoot/admin/delete?id=$product[id]' class=' btn btn-sm btn-outline-danger'>
-                    Tabort
-                </a>
-                <a href='$serverRoot/admin/update?id=$product[id]' class=' btn btn-sm btn-outline-success'>
-                    Redigera
-                </a>
-            </li>";
-    echo $li;
-}
-echo "</ul>";
 
+    $html =
+        "<div class='col-12 d-flex justify-content-between'>
+                <h5>$product[name]</h5>
+                    <div class='row '>
+                            <a href='$serverRoot/admin/delete?id=$product[id]' class='mr-2 btn btn-sm btn-outline-danger'>
+                            Tabort
+                            </a>
+                            <a href='$serverRoot/admin/update?id=$product[id]' class=' btn btn-sm btn-outline-success'>
+                            Redigera
+                            </a>
+                    </div>
+            </div>
+            <hr style='margin:1px,0,10px,0'>";
+    echo $html;
+}
 ?>
