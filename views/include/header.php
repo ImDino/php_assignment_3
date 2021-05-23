e3a8fcbe0af79744b6ca4665504e37816bbdf2a3
Dino Pacariz <dinopacariz@gmail.com>
2021-05-23 17:49:10 +0200

    fixed some indentation in header and removed unused css

--- views/include/header.php
+++ views/include/header.php
@@ -13,31 +13,32 @@
 
     <title><?php echo $title ?> - PHP Butiken</title>
 </head>
+
 <script>
     document.addEventListener("DOMContentLoaded", function(event) { 
-            var scrollpos = localStorage.getItem('scrollpos');
-            if (scrollpos) window.scrollTo(0, scrollpos);
-        });
+        var scrollpos = localStorage.getItem('scrollpos');
+        if (scrollpos) window.scrollTo(0, scrollpos);
+    });
 
-        window.onbeforeunload = function(e) {
-            localStorage.setItem('scrollpos', window.scrollY);
-        };
+    window.onbeforeunload = function(e) {
+        localStorage.setItem('scrollpos', window.scrollY);
+    };
 </script>
+
 <?php
 
 $email = $_SESSION['email'] ?? null;
 $isAdmin = $_SESSION['isAdmin'] ?? null;
 $serverRoot = SERVER_ROOT;
 
-?>
-<?php 
 $numOfItems = 0;
 
-    if (isset($_SESSION['cart'])) {
-        foreach ($_SESSION['cart'] as $product) {
-            $numOfItems += $product;
-        }
+if (isset($_SESSION['cart'])) {
+    foreach ($_SESSION['cart'] as $product) {
+        $numOfItems += $product;
     }
+}
+
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
@@ -48,37 +49,42 @@ $numOfItems = 0;
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav justify-content-between">
         <a class="nav-link mr-1" id="cartIcon" href="<?php echo SERVER_ROOT ?>/cart">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#000000" class="bi bi-cart4" viewBox="0 0 16 16">
-                        <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
-                    </svg>
-                    <span id="cartCount">
-                        <?php echo $numOfItems ?
-                            $numOfItems 
-                            : 
-                            '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                            </svg>';?>
-                    </span>
-                </a>
-            <?php
-            if ($isAdmin) {
-                echo "<li class='nav-item'>
-                        <a class='nav-link' href='$serverRoot/admin'>Admin</a>
-                    </li>";
-            }
-            ?>
-            <?php
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#000000" class="bi bi-cart4" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+            </svg>
+            <span id="cartCount">
+                <?php
+                
+                echo $numOfItems ?
+                    $numOfItems 
+                    : 
+                    '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>';
+                ?>
+            </span>
+        </a>
 
-            echo !$email
-                ?
-                "<li class='nav-item'>
-                    <a class='nav-link' href='$serverRoot/user/login'>Logga in</a>
-                </li>"
-                :
-                "<li class='nav-item'>
-                    <a class='nav-link' href='$serverRoot/user/logout'>Logga ut ($email)</a>
+        <?php
+        if ($isAdmin) {
+            echo "<li class='nav-item'>
+                    <a class='nav-link' href='$serverRoot/admin'>Admin</a>
                 </li>";
-            ?>
+        }
+        ?>
+        <?php
+
+        echo !$email
+            ?
+            "<li class='nav-item'>
+                <a class='nav-link' href='$serverRoot/user/login'>Logga in</a>
+            </li>"
+            :
+            "<li class='nav-item'>
+                <a class='nav-link' href='$serverRoot/user/logout'>Logga ut ($email)</a>
+            </li>";
+        ?>
+
         </ul>
     </div>
 </nav>
